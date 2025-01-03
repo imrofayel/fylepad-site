22f3982002ccabceb337de41c13a919da8eff70e
rofayels <adamrofayel@gmail.com>
2025-01-03 19:49:35 +0500

    SEO 🎁

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
 <template>
   <NuxtLayout>
+
+    <Head>
+      <Title>fylepad — your perfect note-taking solution!</Title>
+      <Meta name="description"
+        content="fylepad is your savior when other apps are too complex for simple tasks or too simple to even manage a basic to-do list. Designed for note-taking and Markdown-based editing, it strikes the perfect balance between simplicity and power." />
+      <Meta name="keywords" content="note-taking, markdown editor, simple notes, productivity, fylepad" />
+      <Meta name="robots" content="index, follow" />
+      <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Meta property="og:title" content="fylepad — your perfect note-taking solution!" />
+      <Meta property="og:description"
+        content="fylepad is your savior when other apps are too complex for simple tasks or too simple to even manage a basic to-do list. Designed for note-taking and Markdown-based editing, it strikes the perfect balance between simplicity and power." />
+      <Meta property="og:type" content="website" />
+      <Meta property="og:image" content="/assets/screenshot3.png" />
+      <Meta name="twitter:card" content="summary_large_image" />
+      <Meta name="twitter:title" content="fylepad — your perfect note-taking solution!" />
+      <Meta name="twitter:description"
+        content="Your perfect note-taking solution with Markdown support, auto-save, and multi-tab editing." />
+      <Link rel="canonical" href="https://github.com/imrofayel/fylepad" />
+    </Head>
     <NuxtPage />
   </NuxtLayout>
 </template>
